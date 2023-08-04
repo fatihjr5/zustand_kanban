aa27b9da080818ce2c76a890410f886dd9494812
Atshilla Fatih Ibrahim <shilla.ibra@gmail.com>
2023-08-04 11:30:43 +0700

    add dynamic title task with modal

--- src/components/column/index.jsx
+++ src/components/column/index.jsx
@@ -1,16 +1,38 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types';
+// Components
+import { Modal, Input } from 'antd'
+// State Management
 import useStore from '../../store';
 import Task from '../task';
 
 function Column({ state }) {
+  // configure state
+  const [text, setText] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // zustand
   const tasks = useStore((store) => store.task.filter((task) => task.state === state));
   const addTasks = useStore((store)=> store.addTask);
+  // configure modal
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  
 
   return (
     <div>
       <div className='border-b border-b-[#292C2D] pb-2 flex flex-row items-center justify-between'>
         <h5 className="text-lg font-semibold text-white">{state}</h5>
-        <button onClick={() => { addTasks('', state) }} className='bg-slate-100 px-2 py-0.5 rounded-sm text-sm font-medium'>Add Task</button>
+        <button onClick={showModal} className='bg-slate-100 px-2 py-0.5 rounded-sm text-sm font-medium'>Tambahkan</button>
+        <Modal title="Tambah Judul" open={isModalOpen} onOk={handleOk,() => {addTasks(text, state)}} onCancel={handleCancel}>
+          <Input placeholder="Enter Title" onChange={(e)=> setText(e.target.value)} value={text}/>
+        </Modal>
       </div>
       {
         tasks.map((item) => (
