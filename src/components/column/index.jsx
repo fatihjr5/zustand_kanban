ce5fff899578354e45256751e7e654f509428cba
Atshilla Fatih Ibrahim <shilla.ibra@gmail.com>
2023-08-04 20:38:17 +0700

    add middleware

--- src/components/column/index.jsx
+++ src/components/column/index.jsx
@@ -35,19 +35,20 @@ function Column({ state }) {
 
   return (
     <div
-    onDragOver={(e) => {
-      setDrop(true);
-      e.preventDefault();
-    }}
-    onDragLeave={(e) => {
-      setDrop(false);
-      e.preventDefault();
-    }}
-    onDrop={(e) => {
-      setDrop(false);
-      moveTask(draggedTask, state);
-      setDraggedTask(null);
-    }}>
+      onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDragLeave={(e) => {
+        setDrop(false);
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        setDrop(false);
+        moveTask(draggedTask, state);
+        setDraggedTask(null);
+      }}
+    >
       <div className='border-b border-b-[#292C2D] pb-2 flex flex-row items-center justify-between'>
         <h5 className="text-lg font-semibold text-white">{state}</h5>
         <button onClick={showModal} className='bg-slate-100 px-2 py-0.5 rounded-sm text-sm font-medium'>Tambahkan</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,25 @@
-/* eslint-disable no-unused-vars */
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-const useStore = create((set) => ({
+const useStore = create(devtools((set) => ({
   // task function
   task: [{ title: "Kanban", state: "Berjalan" }],
   addTask: (title, state) => {
     set((store) => ({ task: [...store.task, { title, state }] }));
   },
   deleteTask: (title) => {
-    set((store) => ({ task: store.task.filter((task)=> task.title !== title) }));
+    set((store) => ({ task: store.task.filter((task) => task.title !== title) }));
   },
   // dragged function
-  draggedTask:null,
+  draggedTask: null,
   setDraggedTask: (title) => set({ draggedTask: title }),
   moveTask: (title, state) =>
     set((store) => ({
       task: store.task.map((task) =>
-        task.title === title ? { title, state } : task
+        task.title === title ? { ...task, state } : task
       ),
-  })),
-}));
+      draggedTask: { ...store.draggedTask, state },
+    })),
+})));
 
 export default useStore;
